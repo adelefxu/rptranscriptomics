d34521a2dfa000c4ff679effc704b25cf6647296
Adele Xu <adele.f.xu@gmail.com>
2020-12-29 00:39:38 -0800

    new versions of run_STAR_RSEM.py and STAR_RSEM.sh for running on meso and endo samples

---
+++ rptranscriptomics/scripts/STAR_RSEM_20201228.sh
@@ -0,0 +1,190 @@
+#!/bin/bash
+
+# AX: Copied from https://github.com/kundajelab/mesoderm/blob/master/RNA-seq/STAR_RSEM.sh
+#
+# AX: changes from original
+# - added code to load necessary modules
+# - removed STARparsMeta from STAR command call b/c never initialized
+# - added command line arguments for output directory and output file prefix (full path)
+# - instead of writing bedgraph output to a Signal subdirectory, just writing directly to output directory
+# - (2020-12-28) outFilterMismatchNoverReadLmax 0.01 instead of 0.04
+# use version of STAR, samtools, RSEM, and R in conda_rptranscriptomics instead of explicitly loading a version
+
+# Modified from https://github.com/ENCODE-DCC/long-rna-seq-pipeline/blob/master/DAC/STAR_RSEM.sh
+# Commit 313830c7c10e8567091131c40bdec2b9477627e0
+
+# STAR mapping / RSEM quantification pipeline
+# usage: run:
+# ./STAR_RSEM.sh (read1) (read2 or "") (STARgenomeDir) (RSEMrefDir) (dataType) (nThreadsSTAR) (nThreadsRSEM) (output directory) (output prefix)
+
+# input: gzipped fastq file read1 [read2 for paired-end] 
+#        STAR genome directory, RSEM reference directory - prepared with STAR_RSEM_prep.sh script
+read1=$1 #gzipped fastq file for read1
+read2=$2 #gzipped fastq file for read1, use "" if single-end
+STARgenomeDir=$3 
+RSEMrefDir=$4 # This needs the "RSEMref" suffix if prepared by the prep script
+dataType=$5 # RNA-seq type, possible values: str_SE str_PE unstr_SE unstr_PE
+nThreadsSTAR=$6 # number of threads for STAR
+nThreadsRSEM=$7 # number of threads for RSEM
+outdir=$8
+outprefix=$9
+
+# change working directory to outdir
+cd $outdir
+
+# output: all in the working directory, fixed names
+# {outprefix}_Aligned.sortedByCoord.out.bam                 # alignments, standard sorted BAM, agreed upon formatting
+# {outprefix}_Log.final.out                                 # mapping statistics to be used for QC, text, STAR formatting
+# {outprefix}_Quant.genes.results                           # RSEM gene quantifications, tab separated text, RSEM formatting
+# {outprefix}_Quant.isoforms.results                        # RSEM transcript quantifications, tab separated text, RSEM formatting
+# {outprefix}_Quant.pdf                                     # RSEM diagnostic plots
+# {outprefix}_Signal.{Unique,UniqueMultiple}.strand{+,-}.bw # 4 bigWig files for stranded data
+# {outprefix}_Signal.{Unique,UniqueMultiple}.unstranded.bw  # 2 bigWig files for unstranded data
+
+# STAR parameters: common
+STARparCommon=" --genomeDir $STARgenomeDir  --readFilesIn $read1 $read2   --outSAMunmapped Within --outFilterType BySJout \
+ --outSAMattributes NH HI AS NM MD    --outFilterMultimapNmax 20   --outFilterMismatchNmax 999   \
+ --outFilterMismatchNoverReadLmax 0.01   --alignIntronMin 20   --alignIntronMax 1000000   --alignMatesGapMax 1000000   \
+ --alignSJoverhangMin 8   --alignSJDBoverhangMin 1 --sjdbScore 1 --readFilesCommand zcat \
+ --twopassMode Basic --twopass1readsN -1 --outFileNamePrefix $outprefix"
+STARparRun=" --runThreadN $nThreadsSTAR --limitBAMsortRAM 60000000000"
+STARparBAM="--outSAMtype BAM SortedByCoordinate --quantMode TranscriptomeSAM"
+
+
+# STAR parameters: strandedness, affects bedGraph (wiggle) files and XS tag in BAM 
+case "$dataType" in
+str_SE|str_PE)
+      #OPTION: stranded data
+      STARparStrand=""
+      STARparWig="--outWigStrand Stranded"
+      ;;
+      #OPTION: unstranded data
+unstr_SE|unstr_PE)
+      STARparStrand="--outSAMstrandField intronMotif"
+      STARparWig="--outWigStrand Unstranded"
+      ;;
+esac
+
+# STAR parameters: metadata
+#STARparsMeta="--outSAMheaderCommentFile commentsENCODElong.txt --outSAMheaderHD @HD VN:1.4 SO:coordinate"
+
+## not needed ## --outSAMheaderPG @PG ID:Samtools PN:Samtools CL:"$samtoolsCommand" PP:STAR VN:0.1.18"
+
+# ENCODE metadata BAM comments
+#echo -e '@CO\tLIBID:ENCLB175ZZZ
+#@CO\tREFID:ENCFF001RGS
+#@CO\tANNID:gencode.v19.annotation.gtf.gz
+#@CO\tSPIKEID:ENCFF001RTP VN:Ambion-ERCC Mix, Cat no. 445670' > commentsENCODElong.txt
+
+###### STAR command
+echo STAR $STARparCommon $STARparRun $STARparBAM $STARparStrand
+
+module load anaconda
+source activate conda_rptranscriptomics
+STAR $STARparCommon $STARparRun $STARparBAM $STARparStrand
+
+###### bedGraph generation, now decoupled from STAR alignment step
+# working subdirectory for this STAR run
+# mkdir Signal
+
+echo STAR --runMode inputAlignmentsFromBAM   --inputBAMfile ${outprefix}Aligned.sortedByCoord.out.bam --outWigType bedGraph $STARparWig --outFileNamePrefix ${outprefix} --outWigReferencesPrefix chr
+STAR --runMode inputAlignmentsFromBAM   --inputBAMfile ${outprefix}Aligned.sortedByCoord.out.bam --outWigType bedGraph $STARparWig --outFileNamePrefix ${outprefix} --outWigReferencesPrefix chr
+
+# move the signal files from the subdirectory
+# mv Signal/${shortprefix}Signal*bg .
+
+
+
+
+###### bigWig conversion commands
+# # exclude spikeins
+# grep ^chr $STARgenomeDir/chrNameLength.txt > chrNL.txt
+
+# case "$dataType" in
+# str_SE|str_PE)
+#       # stranded data
+#       str[1]=-; str[2]=+;
+#       for istr in 1 2
+#       do
+#       for imult in Unique UniqueMultiple
+#       do
+#           grep ^chr Signal.$imult.str$istr.out.bg > sig.tmp
+#           $bedGraphToBigWig sig.tmp  chrNL.txt Signal.$imult.strand${str[istr]}.bw
+#       done
+#       done
+#       ;;
+# unstr_SE|unstr_PE)
+#       # unstranded data
+#       for imult in Unique UniqueMultiple
+#       do
+#           grep ^chr Signal.$imult.str1.out.bg > sig.tmp
+#           $bedGraphToBigWig sig.tmp chrNL.txt  Signal.$imult.unstranded.bw
+#       done
+#       ;;
+# esac
+
+
+
+
+######### RSEM
+
+#### prepare for RSEM: sort transcriptome BAM to ensure the order of the reads, to make RSEM output (not pme) deterministic
+trBAMsortRAM=40G
+
+mv ${outprefix}Aligned.toTranscriptome.out.bam Tr.bam 
+
+case "$dataType" in
+str_SE|unstr_SE)
+      # single-end data
+      cat <( samtools view -H Tr.bam ) <( samtools view -@ $nThreadsRSEM Tr.bam | sort -S $trBAMsortRAM -T ./ ) | samtools view -@ $nThreadsRSEM -bS - > Aligned.toTranscriptome.out.bam
+      ;;
+str_PE|unstr_PE)
+      # paired-end data, merge mates into one line before sorting, and un-merge after sorting
+      cat <( samtools view -H Tr.bam ) <( samtools view -@ $nThreadsRSEM Tr.bam | awk '{printf "%s", $0 " "; getline; print}' | sort -S $trBAMsortRAM -T ./ | tr ' ' '\n' ) | samtools view -@ $nThreadsRSEM -bS - > ${outprefix}Aligned.toTranscriptome.out.bam
+      ;;
+esac
+
+'rm' Tr.bam
+
+
+# RSEM parameters: common
+# RSEMparCommon="--bam --estimate-rspd  --calc-ci --no-bam-output --seed 12345"
+RSEMparCommon="--bam --estimate-rspd --no-bam-output --seed 12345"
+
+# RSEM parameters: run-time, number of threads and RAM in MB
+# RSEMparRun=" -p $nThreadsRSEM --ci-memory 40000 "
+RSEMparRun=" -p $nThreadsRSEM "
+
+# RSEM parameters: data type dependent
+
+case "$dataType" in
+str_SE)
+      #OPTION: stranded single end
+      RSEMparType="--forward-prob 0"
+      ;;
+str_PE)
+      #OPTION: stranded paired end
+      RSEMparType="--paired-end --forward-prob 0"
+      ;;
+unstr_SE)
+      #OPTION: unstranded single end
+      RSEMparType=""
+      ;;
+unstr_PE)
+      #OPTION: unstranded paired end
+      RSEMparType="--paired-end"
+      ;;
+esac
+
+
+###### RSEM command
+echo rsem-calculate-expression $RSEMparCommon $RSEMparRun $RSEMparType ${outprefix}Aligned.toTranscriptome.out.bam $RSEMrefDir ${outprefix}Quant >& ${outprefix}Log.rsem
+rsem-calculate-expression $RSEMparCommon $RSEMparRun $RSEMparType ${outprefix}Aligned.toTranscriptome.out.bam $RSEMrefDir ${outprefix}Quant >& ${outprefix}Log.rsem
+
+###### RSEM diagnostic plot creation
+# Notes:
+# 1. rsem-plot-model requires R (and the Rscript executable)
+# 2. This command produces the file Quant.pdf, which contains multiple plots
+
+echo rsem-plot-model ${outprefix}Quant ${outprefix}Quant.pdf
+rsem-plot-model ${outprefix}Quant ${outprefix}Quant.pdf
---
+++ rptranscriptomics/scripts/run_STAR_RSEM_meso_20201228.py
@@ -0,0 +1,60 @@
+# before running this script:
+# module load anaconda
+# git commit 
+# run from directory in which script is stored; otherwise git query won't work
+
+import os
+import subprocess
+import argparse
+import glob
+import json
+
+parser = argparse.ArgumentParser(description="Run STAR and RSEM on all .fastq.gz files found in subdirectories of directory provided.")
+parser.add_argument('--trimmed_dir')
+parser.add_argument('--out_dir')
+parser.add_argument('--log_dir')
+parser.add_argument('--data_type', default='str_PE')
+parser.add_argument('--SR_script', default="/home/adelexu/research/rptranscriptomics/rptranscriptomics/scripts/STAR_RSEM_20201228.sh")
+parser.add_argument('--STAR_genome_dir', default="/labs/mbarna/users/adelexu/rptranscriptomics/genomes/gencode.v22_ERCC/STAR_genome")
+parser.add_argument('--RSEM_out_prefix', default="/labs/mbarna/users/adelexu/rptranscriptomics/genomes/gencode.v22_ERCC/RSEM_genome/RSEM_genome")
+parser.add_argument("--sb_name")
+parser.add_argument("--sb_time", default = "12:0:0")
+parser.add_argument("--sb_mem", default = "64G")
+parser.add_argument("--sb_cpus", default = "2")
+parser.add_argument("--exclude", nargs=argparse.REMAINDER, default='none')
+args = parser.parse_args()
+
+# make sure output and log directories exist
+os.makedirs(args.out_dir, exist_ok=True)
+os.makedirs(args.log_dir, exist_ok=True)
+
+sample = next(os.walk(args.trimmed_dir))[1]
+
+#exclude the pilot
+sample = [s for s in sample if s not in args.exclude]
+
+nThreadsSTAR = args.sb_cpus
+nThreadsRSEM = args.sb_cpus
+
+sb_defaults = f"-e {args.log_dir}/%j-%x.e -o {args.log_dir}/%j-%x.o -A mbarna -p batch"
+
+# for each sample, make output subdirectory and call STAR_RSEM script
+for smp in sample:
+    smp_dir = os.path.join(args.out_dir, smp)
+    os.makedirs(smp_dir, exist_ok=True)
+    pair1 = glob.glob(os.path.join(args.trimmed_dir, smp, "*-trimmed-pair1.fastq.gz"))[0]
+    pair2 = glob.glob(os.path.join(args.trimmed_dir, smp, "*-trimmed-pair2.fastq.gz"))[0]
+    
+    call_script = f"{args.SR_script} {pair1} {pair2} {args.STAR_genome_dir} {args.RSEM_out_prefix} {args.data_type} {nThreadsSTAR} {nThreadsRSEM} {smp_dir} {os.path.join(smp_dir, smp+'_')}"
+    
+    sb_cmd = f"sbatch {sb_defaults} -D {args.log_dir} -J {smp}_{args.sb_name} -t {args.sb_time} --mem={args.sb_mem} -c {args.sb_cpus} {call_script}"
+    sb_sub_msg = subprocess.check_output(sb_cmd, shell=True).decode('ascii')
+    print(sb_sub_msg)
+    job_id = sb_sub_msg.strip().replace("Submitted batch job ", "")
+ 
+#    uncomment when running from command line
+    with open(os.path.join(args.log_dir, f"{smp}_{args.sb_name}_STAR_RSEM_{job_id}.config"), 'w') as config_file:
+        git_version = str(subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip())
+        configs = {"git version": git_version, "arguments": vars(args), "sbatch command line": sb_cmd}
+        json.dump(configs, config_file, indent=4)
+
